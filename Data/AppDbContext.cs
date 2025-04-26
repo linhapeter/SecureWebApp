ccd360d27d265498af69a5f486021300f2a40b1d
linhapeter <linha.peter.jr@gmail.com>
2025-04-26 14:10:33 +0200

    DbContext creation and registration

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SecureWebApp.Models;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<User> Users { get; set; }
+}
