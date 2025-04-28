2c47e74216742e6ca9bad6f27ce7dcbaf8f53765
linhapeter <linha.peter.jr@gmail.com>
2025-04-28 08:36:27 +0200

    Created Users table and established connection with local server

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -3,7 +3,18 @@ using SecureWebApp.Models;
 namespace SecureWebApp.Data;
 public class AppDbContext : DbContext
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public IConfiguration _config { get; init; }
+
+    public AppDbContext(IConfiguration config)
+    {
+        _config = config;
+    }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(_config.GetConnectionString("DatabaseConnection"));
+    }
 
     public DbSet<User> Users { get; set; }
 }
