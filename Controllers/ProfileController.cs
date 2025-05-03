82d12c63be85c99495ade3d61d64abbb4bb00334
linhapeter <linha.peter.jr@gmail.com>
2025-05-03 12:09:54 +0200

    Profile view and password change functionality.

---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SecureWebApp.Data;
+using SecureWebApp.Models;
+
+public class ProfileController : Controller
+{
+    private readonly AppDbContext _context;
+
+    public ProfileController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    // No authentication check - anyone can access
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    // Password change with ZERO security
+    [HttpPost]
+    public IActionResult ChangePassword(string newPassword)
+    {
+        
+        var username = Request.Cookies["Username"];
+
+        
+        var user = _context.Users.FirstOrDefault(u => u.UserName == username);
+        if (user != null)
+        {
+            user.Password = newPassword; 
+            _context.SaveChanges();
+
+            
+            ViewBag.Message = "Password changed";
+        }
+
+        return View("Index");
+    }
+}
